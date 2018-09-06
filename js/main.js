5b128b87170cf01aa9af14dec3f7c2f4585010ab
Shalin Mehta <shalinmehta@Shalins-MacBook-Air.local>
2018-09-07 00:57:15 +0530

    Updates

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// Select DOM items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set Initial State of Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(i => i.classList.add("show"));
+
+    // Reset Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(i => i.classList.remove("show"));
+
+    // Reset Menu State
+    showMenu = false;
+  }
+}
+
+// Smooth Scrolling
+$(".cf a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
