2469bb15f99b947d5b06ac52a2ad0e3d151798e3
Shalin Mehta <shalinmehta@Shalins-MacBook-Air.local>
2018-09-06 21:34:49 +0530

    Menu Overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,22 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// Smooth Scrolling
+$(".cf a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
